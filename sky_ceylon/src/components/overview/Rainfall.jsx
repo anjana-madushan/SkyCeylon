1aa893523731ef66dc214b958f9a2e212b1b5539
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2025-06-16 14:16:20 +0530

    ui: implement rainfall section ui with level and rainfall value in mm

--- sky_ceylon/src/components/overview/Rainfall.jsx
+++ sky_ceylon/src/components/overview/Rainfall.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { BsCloudRain } from "react-icons/bs";
+import { getRainfallInfo } from "../../util/rainfallInfo";
 
 const Rainfall = () => {
   return (
-    <div className="bg-gray-100 border-none rounded-2xl">Rainfall</div>
+    <div className="flex flex-col bg-gray-100 border-none rounded-2xl px-8 py-2 gap-8">
+      <div className="flex items-center gap-2">
+        <BsCloudRain />
+        <span className="text-gray-500">Rainfall</span>
+      </div>
+      <div className="flex flex-col gap-2 items-center">
+        <p className="text-3xl">25.27 mm</p>
+        <p className="text-gray-400 text-sm text-center">Expect {getRainfallInfo(25.27)} today.</p>
+      </div>
+    </div>
   )
 }
 
---
+++ sky_ceylon/src/util/rainfallInfo.jsx
@@ -0,0 +1,19 @@
+// Based on the precip_mm, rainfall level will be returned.
+
+export const getRainfallInfo = (precip_mm) => {
+  if (precip_mm == 1) {
+    return 'No rain';
+  } else if (precip_mm < 1) {
+    return 'Very Light Rain';
+  } else if (precip_mm < 10) {
+    return 'Light Rain';
+  } else if (precip_mm < 31) {
+    return 'Moderate Rain';
+  } else if (precip_mm < 71) {
+    return 'Heavy Rain';
+  } else if (precip_mm < 151) {
+    return 'Very Heavy Rain';
+  } else {
+    return 'Extremely Heavy Rain';
+  }
+};
