10eb1ecaee1a78edd51b14ef4e8c8643671f548e
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2025-06-17 23:49:36 +0530

    ui-bug: removed the index level based color changing issue with the horizontal bar

--- sky_ceylon/src/components/overview/AirQuality.jsx
+++ sky_ceylon/src/components/overview/AirQuality.jsx
@@ -2,22 +2,22 @@ import { GiSmokeBomb } from "react-icons/gi";
 
 const AirQuality = () => {
 
-  const airIndex = 4;
+  const airIndex = 1;
   const maxIndex = 6;
 
   const getAirQualityInfo = (index) => {
     if (index === 1) {
-      return { level: 'Good', description: 'Air quality is clean and healthy.', color: 'green-300' };
+      return { level: 'Good', description: 'Air quality is clean and healthy.' };
     } else if (index === 2) {
-      return { level: 'Moderate', description: 'Acceptable, but some pollutants may affect sensitive people.', color: 'yellow-300' };
+      return { level: 'Moderate', description: 'Acceptable, but some pollutants may affect sensitive people.' };
     } else if (index === 3) {
-      return { level: 'Unhealthy for Sensitive Groups', description: 'Sensitive groups may feel mild effects.', color: 'orange-300' };
+      return { level: 'Unhealthy for Sensitive Groups', description: 'Sensitive groups may feel mild effects.' };
     } else if (index === 4) {
-      return { level: 'Unhealthy', description: 'Everyone may begin to feel effects.', color: 'red-300' };
+      return { level: 'Unhealthy', description: 'Everyone may begin to feel effects.' };
     } else if (index === 5) {
-      return { level: 'Very Unhealthy', description: 'Health alert: serious effects possible.', color: 'purple-300' };
+      return { level: 'Very Unhealthy', description: 'Health alert: serious effects possible.' };
     } else if (index === 6) {
-      return { level: 'Hazardous', description: 'Emergency: avoid outdoor exposure completely.', color: 'maroon-300' };
+      return { level: 'Hazardous', description: 'Emergency: avoid outdoor exposure completely.' };
     } else {
       return { level: '', description: '', color: '' };
     }
@@ -31,14 +31,19 @@ const AirQuality = () => {
         <p className="text-gray-500">Air Quality</p>
       </div>
       <div className="flex flex-col items-center">
-        <p className={`text-sm font-semibold mb-2`}>{airIndex} / {maxIndex}</p>
+        <p className='text-sm font-semibold mb-2 text-blue-400'>{airIndex} / {maxIndex}</p>
         <div className="w-full bg-gray-200 rounded-full h-4">
           <div
-            className={`h-4 rounded-full bg-${getAirQualityInfo(4).color}`}
+            className='h-4 rounded-full bg-blue-400'
             style={{ width: `${airIndex > maxIndex ? (100) : (airIndex / maxIndex) * 100}%` }}
           />
         </div>
-        <p className={`text-center mt-2 font-semibold`}>{getAirQualityInfo(airIndex).level}</p>
+        <div className="w-full flex justify-between px-1 text-xs text-gray-500 my-1">
+          {[0, 2, 4, 6].map((tick, i) => (
+            <span key={i}>{tick}</span>
+          ))}
+        </div>
+        <p className='text-center mt-2 text-blue-400'>{getAirQualityInfo(airIndex).level}</p>
         <p className="text-gray-400 text-sm text-center mt-2">{getAirQualityInfo(airIndex).description}</p>
       </div>
     </div>
--- sky_ceylon/src/components/overview/UV.jsx
+++ sky_ceylon/src/components/overview/UV.jsx
@@ -7,31 +7,30 @@ const UV = () => {
   const uvIndex = 10;
   const uvInfo = getUVLevelInfo(uvIndex);
 
-  const getColor = (value) => {
-    if (value < 3) return 'green-500';
-    if (value < 6) return 'yellow-400';
-    if (value < 8) return 'orange-500';
-    if (value < 11) return 'red-500';
-    return 'purple-700';
-  };
-
   return (
     <div className='flex flex-col bg-gray-100 border-none rounded-2xl px-8 py-2 gap-3'>
       <div className="flex justify-between mb-1">
         <div className="flex items-center gap-2">
           <WiDaySunny />
-          <span className="text-gray-500">UV Index</span>
+          <p className="text-gray-500">UV Index</p>
         </div>
-        <span className={`text-sm text-${getColor(uvIndex)} font-semibold`}>{uvIndex} / {maxUV}</span>
       </div>
-      <div className="w-full bg-gray-200 rounded-full h-4">
-        <div
-          className={`h-4 rounded-full bg-${getColor(uvIndex)}`}
-          style={{ width: `${uvIndex > 11 ? (100) : (uvIndex / maxUV) * 100}%` }}
-        />
+      <div className="flex flex-col items-center">
+        <p className='text-sm text-blue-400 mb-2 font-semibold'>{uvIndex} / {maxUV}</p>
+        <div className="w-full bg-gray-200 rounded-full h-4">
+          <div
+            className='h-4 rounded-full bg-blue-400'
+            style={{ width: `${uvIndex > 11 ? (100) : (uvIndex / maxUV) * 100}%` }}
+          />
+        </div>
+        <div className="w-full flex justify-between px-1 text-xs text-gray-500 my-1">
+          {[0, 3, 6, 9, 12].map((tick, i) => (
+            <span key={i}>{tick}</span>
+          ))}
+        </div>
+        <p className='text-center text-blue-400 mt-2'>{uvInfo.level}</p>
+        <p className="text-gray-400 text-sm text-center mt-2">{uvInfo.description}</p>
       </div>
-      <p className={`text-center text-${getColor(uvIndex)}`}>{uvInfo.level}</p>
-      <p className="text-gray-400 text-sm text-center">{uvInfo.description}</p>
     </div>
   );
 }
