618b1c818d84aebd10deeb1ac13b8c782b4bbde6
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2025-06-23 10:46:15 +0530

    style: remove z-index from Today Forecast sticky header to prevent overlap issues

--- sky_ceylon/src/components/Forcast/Today.jsx
+++ sky_ceylon/src/components/Forcast/Today.jsx
@@ -4,7 +4,7 @@ const Today = ({ forcast }) => {
 
   return (
     <div className='flex flex-col bg-gray-100 border-none rounded-2xl px-4 pt-2 pb-3 gap-3'>
-      <p className='text-gray-500 sticky top-0 bg-gray-100 z-10'>Today Forcast</p>
+      <p className='text-gray-500 sticky top-0 bg-gray-100'>Today Forcast</p>
       {forcast && forcast.map((item, index) => (
         <ForcastCard key={index}
           humidity={item?.humidity}
