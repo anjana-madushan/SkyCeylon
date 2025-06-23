0328004f65ba1245da0cf01aa1fd2f738d7d765e
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2025-06-24 02:23:16 +0530

    style: change the gap between the humitdity and cloud cover

--- sky_ceylon/src/components/Dashboard.jsx
+++ sky_ceylon/src/components/Dashboard.jsx
@@ -69,7 +69,7 @@ const Dashboard = () => {
                         feelslike_f={currentWeather?.feelslike_f}
                         timestamp={locationDetails?.localtime} />
                     </div>
-                    <div className="flex flex-col gap-2">
+                    <div className="flex flex-col justify-between">
                       <Humidity value={currentWeather?.humidity} />
                       <CloudCover value={currentWeather?.cloud} />
                     </div>
